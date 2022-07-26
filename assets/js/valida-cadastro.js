fb8c0fb635da219b129f9bf7bc68af7c70036ebc
artenlf <felipe.arten@gmail.com>
2022-07-25 22:29:49 -0300

    Adicionando validação e tratamento de erros no cadastro

--- assets/js/valida-cadastro.js
+++ assets/js/valida-cadastro.js
@@ -1,9 +1,71 @@
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
+
+function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('.formulario__campo-invalido')
+        input.parentElement.querySelector('.formulario__mensagem-erro').innerHTML = '';
+        input.style.border = 'none';
+    } else {
+        input.parentElement.classList.add('.formulario__campo-invalido');
+        input.parentElement.querySelector('.formulario__mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+        input.style.border = '2px solid red';
+    }
+}
+
+const validadores = {
+    senha: input => validaSenha(input),
+    confirmasenha: input => validaSenha(input)
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+];
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo e-mail não pode estar vazio.',
+        typeMismatch: 'O e-mail digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e não deve conter símbolos.'
+    },
+    confirmasenha: {
+        valueMissing: 'O campo confirme sua senha não pode estar vazio.',
+        customError: 'As senhas digitadas devem ser iguais.'
+    }
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = '';
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro];
+        }
+    })
+    return mensagem;
+}
+
 const senha = document.getElementById('senha');
 const confirmaSenha = document.getElementById('confirmasenha');
 
-function validaSenha() {
+function validaSenha(input) {
     if (senha.value !== confirmaSenha.value) {
-        confirmaSenha.setCustomValidity('As senhas digitadas devem ser iguais.');        
+        confirmaSenha.setCustomValidity('As senhas digitadas devem ser iguais.');
     } else {
         confirmaSenha.setCustomValidity('');
     }
