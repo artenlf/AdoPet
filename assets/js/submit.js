151c36466728ad107cc191812d8bf3281b2031c2
artenlf <felipe.arten@gmail.com>
2022-09-08 14:55:38 -0300

    cadastro e login funcionais localstorage

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,31 +1,33 @@
-const button = document.getElementById('submit');
-
+const button = document.getElementById("submit");
 const addPreLoader = () => {
-    button.innerHTML = '<img src="assets/img/loading.png"class="loading">'
-}
-
-const removePreLoader = () => {
-    button.innerHTML = 'Cadastrar'
-}
-
-const processaSubmit = (evento) => {
-    evento.preventDefault();
-    addPreLoader();
-
-    const nome = document.querySelector('input[name=nome]').value;
-    const email = document.querySelector('input[name=email]').value;
-    const senha = document.querySelector('input[name=senha]').value;
-
-    fetch('https://api.sheetmonkey.io/form/b3mfMkWQY4knHztCZ1ACSi', {
-
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-
-        body: JSON.stringify({ nome, email, senha })
-    }).then(() => removePreLoader());
+  button.innerHTML = '<img src="assets/img/loading.png" class="loading">';
 };
 
-document.getElementById('formulario').addEventListener('submit', processaSubmit);
+const removePreLoader = () => {
+  button.innerHTML = "Cadastrar";
+};
+
+const processaSubmit = (evento) => {
+  evento.preventDefault();
+  addPreLoader();
+
+  const nome = document.querySelector("input[name=nome]").value;
+  const email = document.querySelector("input[name=email]").value;
+  const senha = document.querySelector("input[name=senha]").value;
+
+  const usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+  usuarios.push({
+    nomeCadastro: nome,
+    emailCadastro: email,
+    senhaCadastro: senha,
+  });
+
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+  setTimeout(() => {
+    window.location.href = "login.html";
+  }, 2500);
+};
+
+document.getElementById("formulario").addEventListener("submit", processaSubmit);
---
+++ assets/js/valida-login.js
@@ -0,0 +1,52 @@
+const button = document.getElementById("submit");
+const addPreLoader = () => {
+  button.innerHTML = '<img src="assets/img/loading.png" class="loading">';
+};
+
+const removePreLoader = () => {
+  button.innerHTML = "Cadastrar";
+};
+
+const processaLogin = (evento) => {
+  evento.preventDefault();
+  addPreLoader();
+
+  const email = document.querySelector("input[name=email]");
+  const senha = document.querySelector("input[name=senha]");
+  const campoErro = document.querySelector(".formulario__mensagem-erro");
+
+  listaUsuarios = [];
+
+  let validaUsuario = {
+    nome: "",
+    email: "",
+    senha: "",
+  };
+
+  listaUsuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+  listaUsuarios.forEach((usuario) => {
+    if (email.value == usuario.emailCadastro && senha.value == usuario.senhaCadastro) {
+      validaUsuario = {
+        nome: usuario.nomeCadastro,
+        email: usuario.emailCadastro,
+        senha: usuario.senhaCadastro,
+      };
+    }
+  });
+
+  if (email.value == validaUsuario.email && senha.value == validaUsuario.senha) {
+    campoErro.innerHTML = "";
+    setTimeout(() => {
+      window.location.href = "animais.html";
+    }, 2500);
+  } else {
+    campoErro.innerHTML = "Usuário/Senha inválido(s)";
+    email.setAttribute("style", "color: red");
+    senha.setAttribute("style", "color: red");
+    removePreLoader();
+    email.focus();
+  }
+};
+
+document.getElementById("formulario").addEventListener("submit", processaLogin);
