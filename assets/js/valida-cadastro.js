5752a4fe1e87fa98f7e486df74921feae5a4feab
artenlf <felipe.arten@gmail.com>
2022-09-08 08:08:41 -0300

    Melhorando tratamento de erros

--- assets/js/valida-cadastro.js
+++ assets/js/valida-cadastro.js
@@ -1,76 +1,71 @@
-const inputs = document.querySelectorAll('input')
+const inputs = document.querySelectorAll("input");
 
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target)
-    })
-})
+inputs.forEach((input) => {
+  input.addEventListener("blur", (evento) => {
+    valida(evento.target);
+  });
+});
 
 function valida(input) {
-    const tipoDeInput = input.dataset.tipo;
+  const tipoDeInput = input.dataset.tipo;
 
-    if (input.validity.valid) {
-        input.parentElement.classList.remove('.formulario__campo-invalido')
-        input.parentElement.querySelector('.formulario__mensagem-erro').innerHTML = '';
-        input.style.border = 'none';
-    } else {
-        input.parentElement.classList.add('.formulario__campo-invalido');
-        input.parentElement.querySelector('.formulario__mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input);
-        input.style.border = '2px solid red';
-    }
+  if (input.validity.valid) {
+    input.parentElement.classList.remove(".formulario__campo-invalido");
+    input.parentElement.querySelector(".formulario__mensagem-erro").innerHTML = "";
+    input.style.border = "none";
+  } else {
+    input.parentElement.classList.add(".formulario__campo-invalido");
+    input.parentElement.querySelector(".formulario__mensagem-erro").innerHTML = mostraMensagemDeErro(tipoDeInput, input);
+    input.style.border = "2px solid red";
+  }
 }
 
 const validadores = {
-    senha: input => validaSenha(input),
-    confirmasenha: input => validaSenha(input)
-}
+  senha: (input) => validaSenha(input),
+  confirmasenha: (input) => validaSenha(input),
+};
 
-const tiposDeErro = [
-    'valueMissing',
-    'typeMismatch',
-    'patternMismatch',
-    'customError'
-];
+const tiposDeErro = ["valueMissing", "typeMismatch", "patternMismatch", "customError"];
 
 const mensagensDeErro = {
-    nome: {
-        valueMissing: 'O campo nome não pode estar vazio.'
-    },
-    email: {
-        valueMissing: 'O campo e-mail não pode estar vazio.',
-        typeMismatch: 'O e-mail digitado não é válido.'
-    },
-    senha: {
-        valueMissing: 'O campo senha não pode estar vazio.',
-        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, pelo menos uma letra maiúscula, uma letra minúscula, um número e não deve conter espaços.'
-    },
-    confirmasenha: {
-        valueMissing: 'O campo confirme sua senha não pode estar vazio.',
-        customError: 'As senhas digitadas devem ser iguais.'
-    }
-}
+  nome: {
+    valueMissing: "O campo nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo e-mail não pode estar vazio.",
+    typeMismatch: "O e-mail digitado não é válido.",
+  },
+  senha: {
+    valueMissing: "O campo senha não pode estar vazio.",
+    patternMismatch: "Senha de 6 a 12 caracteres, uma letra maiúscula, uma letra minúscula, um número.",
+  },
+  confirmasenha: {
+    valueMissing: "O campo confirme sua senha não pode estar vazio.",
+    customError: "As senhas digitadas devem ser iguais.",
+  },
+};
 
 function mostraMensagemDeErro(tipoDeInput, input) {
-    let mensagem = '';
-    tiposDeErro.forEach(erro => {
-        if (input.validity[erro]) {
-            mensagem = mensagensDeErro[tipoDeInput][erro];
-        }
-    })
-    return mensagem;
+  let mensagem = "";
+  tiposDeErro.forEach((erro) => {
+    if (input.validity[erro]) {
+      mensagem = mensagensDeErro[tipoDeInput][erro];
+    }
+  });
+  return mensagem;
 }
 
-const senha = document.getElementById('senha');
-const confirmaSenha = document.getElementById('confirmasenha');
+const senha = document.getElementById("senha");
+const confirmaSenha = document.getElementById("confirmasenha");
 
-senha.pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ ]).{6,12}$"
+senha.pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[ ]).{6,12}$";
 
 function validaSenha(input) {
-    if (senha.value !== confirmaSenha.value) {
-        confirmaSenha.setCustomValidity('As senhas não conferem.');
-    } else {
-        confirmaSenha.setCustomValidity('');
-    }
+  if (senha.value !== confirmaSenha.value) {
+    confirmaSenha.setCustomValidity("As senhas não conferem.");
+  } else {
+    confirmaSenha.setCustomValidity("");
+  }
 }
 
 senha.onchange = validaSenha;
