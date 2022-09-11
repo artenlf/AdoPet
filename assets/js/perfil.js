ff8501a8ba9ac8db2afd7300b4ca51c24db2b7c2
artenlf <felipe.arten@gmail.com>
2022-09-10 23:58:32 -0300

    Alterando funcionalidades página perfil

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,4 +1,22 @@
 let usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
-let nome = document.querySelector("#nome");
+let nome = document.getElementById("nome");
+let telefone = document.getElementById("telefone");
+let cidade = document.getElementById("cidade");
+let sobre = document.getElementById("sobre");
 
 nome.placeholder = usuarioLogado.nome;
+
+let perfil = {
+  ...usuarioLogado,
+  telefone: "",
+  cidade: "",
+  sobre: "",
+};
+
+localStorage.setItem("perfil", JSON.stringify(perfil));
+
+const salvar = (evento) => {
+  evento.preventDefault();
+};
+
+document.getElementById("formulario").addEventListener("submit", salvar);
